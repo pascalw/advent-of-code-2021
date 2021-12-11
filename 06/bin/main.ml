93baecfcb2bc2954115f0b641450466c1d7d4bd8
Pascal Widdershoven <hello@pascalw.me>
2021-12-11 23:33:13 +0100

    Add day 6 part 1 in OCaml

---
+++ 06/bin/main.ml
@@ -0,0 +1,45 @@
+open Core
+
+module Fish = struct
+  type t = { timer : int }
+
+  let age t = match t with
+  | { timer = 0 } -> [ { timer = 6 } ; { timer = 8 } ]
+  | t -> [ { timer = t.timer - 1 } ]
+
+  let from_string value =
+    let timer = Int.of_string value in
+    { timer }
+
+  let to_string f = Printf.sprintf "Fish{ timer: %d }" f.timer
+end
+
+module Simulation = struct
+  type t = { fishes : Fish.t list }
+
+  let make fishes = { fishes }
+
+  let tick' t =
+    let next_fishes = List.map t.fishes ~f: Fish.age in
+    { fishes = Caml.List.flatten next_fishes }
+
+  let rec tick t n = match n with
+  | 0 -> t
+  | _ -> tick (tick' t) (n - 1)
+end
+
+module Input = struct
+  let read = In_channel.read_lines
+  let split = String.split ~on: ','
+
+  let parse file_contents =
+    let values = Caml.List.flatten (List.map file_contents ~f:split) in
+    List.map values ~f:Fish.from_string
+end
+
+let () =
+  let input = Input.read "input" in
+  let fishes = Input.parse input in
+  let simulation = Simulation.make fishes in
+  let simulation = Simulation.tick simulation 80 in
+  print_endline (string_of_int (List.length simulation.fishes)) ;;
